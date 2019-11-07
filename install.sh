d21c2a61bdba49b70ba71501f50c962b31f0bb2a
weijing24 <645509024@qq.com>
2019-11-07 17:07:28 +0800

    Scripted auto-commit on change (2019-11-07 17:07:28) by gitwatch.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,7 @@ apt install vim nodejs autojump inotify-tools supervisor
 echo "======= install fzf ========"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && cd .fzf && ./install
 echo "======= install bat ========"
-curl https://github.com/sharkdp/bat/releases/download/v0.12.1/
-bat_0.12.1_amd64.deb -o /root/bat_0.12.1_amd64.deb && dpkg -i /root/bat_0.12.1_amd64.deb
+curl https://github.com/sharkdp/bat/releases/download/v0.12.1/bat_0.12.1_amd64.deb -o /root/bat_0.12.1_amd64.deb | dpkg -i /root/bat_0.12.1_amd64.deb
 echo "======= set timezone ========"
 timedatectl set-timezone Asia/Shanghai
 echo "======= set startup ========"
