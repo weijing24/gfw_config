1bff6b63be1388582e3b82152b0e6a0febbf02b0
root <root@fuck-gfw-hk.asia-east2-a.c.abiding-window-257311.internal>
2020-07-07 08:55:22 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 ## Ubuntu install script
 ```
 add-apt-repository ppa:jonathonf/vim
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+#curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 apt update
-apt install vim nodejs autojump inotify-tools supervisor
+apt install -y vim autojump inotify-tools supervisor
 echo "======= install fzf ========"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && cd .fzf && ./install
 echo "======= install bat ========"
-curl https://github.com/sharkdp/bat/releases/download/v0.12.1/bat_0.12.1_amd64.deb -o /root/bat_0.12.1_amd64.deb | dpkg -i /root/bat_0.12.1_amd64.deb
+curl -L https://github.com/sharkdp/bat/releases/download/v1.12.1/bat_0.12.1_amd64.deb -o /root/bat_0.12.1_amd64.deb | dpkg -i /root/bat_0.12.1_amd64.deb
 echo "======= set timezone ========"
 timedatectl set-timezone Asia/Shanghai
 echo "======= set startup ========"
