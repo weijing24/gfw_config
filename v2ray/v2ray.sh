ff796f30a23264b2571ce2c9bd84071525aaa55d
Jim Wei <645509024@qq.com>
2022-09-27 14:55:29 +0800

    Update v2ray.sh

--- v2ray/v2ray.sh
+++ v2ray/v2ray.sh
@@ -1,1 +1,1 @@
-docker run -idt --name v2ray -v /etc/v2ray/:/etc/v2ray -p 443:10086 v2ray/official
+docker run -idt --restart=always --name v2ray -v /root/v2ray/config.json:/etc/v2ray/config.json -p 8489:10086 v2fly/v2fly-core:v4.44.0
